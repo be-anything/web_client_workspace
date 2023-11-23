77644123f88c25098850a50e30bb4d27730c2745
minjeong <snailmin27@gmail.com>
2023-11-23 23:47:32 +0900

    HTML_JS_실습문제

---
+++ 03_javascript/js/실습문제_이벤트.js
@@ -0,0 +1,19 @@
+// 이벤트 생성하기 btn-generate click
+document.querySelector('#btn-generate').onclick = (e) => {
+    console.log(e);
+    
+    const btnInfo = `네이버,https://www.naver.com
+구글,https://www.google.com
+W3C,https://www.w3.org/
+MDN Web,https://developer.mozilla.org/en-US/
+와삼,https://www.w3schools.com`;
+
+    const [...btnInfos] = btnInfo.split('\n');
+    
+    btnInfos.forEach((btn) => {
+        const [homepage, url] = btn.split(',');
+        // button만들기
+        const area = document.querySelector('.btn-wrapper');
+        area.innerHTML += `<button type="button" onclick="window.open('${url}', '_self')">${homepage}</button>`;
+    });
+};
