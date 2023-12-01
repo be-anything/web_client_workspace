e7ac06e2589c3fbfac35e108dc3eff77a0a7156c
minjeong <snailmin27@gmail.com>
2023-12-01 23:59:17 +0900

    HTML_jQuery DOM | selector | extends

---
+++ 04_jquery/js/08_minQuery.js
@@ -0,0 +1,13 @@
+console.log($("#title"));
+
+$("#title").css('color', 'blue');
+
+$("li")
+    .css('text-decoration', 'underline')
+    .css('background-color', 'lightblue');
+
+$("#title").click(() => {
+    alert('반갑습니다~🎄🎄🎄')
+});
+
+$("li").html('졸려용🎵');
